d14993aa93960cb0d324f7e2f3937cb10013f838
Atandrit Chatterjee <chatteat@amdocs.com>
2026-04-01 20:11:04 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const header = document.querySelector('header');
+const heroHeading = document.querySelector('#hero h2');
+const allCards = document.querySelectorAll('.project-card');
+
+console.log(header);
+console.log(heroHeading);
+console.log(allCards);
